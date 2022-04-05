a756e22486fadf8aa94c48b614d4fece8b06b898
Igor <92385445+ITerekhov@users.noreply.github.com>
2022-04-05 14:27:36 +0300

    Внёс правки согласно код-ревью

--- lesson_2.py
+++ lesson_2.py
@@ -16,27 +16,36 @@ def count_clicks(token, link:str):
     formated_link = link.netloc + link.path
     request_url = f'https://api-ssl.bitly.com/v4/bitlinks/{formated_link}/clicks/summary'
     requests_headers = {'Authorization': f'Bearer {token}'}
-    clicks_count = requests.get(request_url, headers=requests_headers, params={'units': -1})
-    clicks_count.raise_for_status()
-    response = f"Данной ссылкой воспользовались {clicks_count.json()['total_clicks']} раз"
-    return response
+    response = requests.get(request_url, headers=requests_headers, params={'units': -1})
+    response.raise_for_status()
+    click_count = response.json()['total_clicks']
+    return click_count
 
-def is_bitlink(url):
+def is_bitlink(url, token):
+    '''Функция делает запрос на указанный url чтобы убедится в его валидности, 
+       а после делает запрос к API сервиса, чтобы узнать, является ли url уже 
+       сокращенной ссылкой'''
+    requests_headers = {'Authorization': f'Bearer {token}'}
+    check_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
+    response = requests.get(url)
+    response.raise_for_status()
     url = urlparse(url)
-    if url.netloc == 'bit.ly':
-        return True
-    return False
+    bitlink = f'{url.netloc}{url.path}'
+    response = requests.get(check_url.format(bitlink), headers=requests_headers)
+    return response.ok
+
     
 if __name__ == '__main__':
     load_dotenv()
     TOKEN = os.getenv("BITLY_TOKEN")
     url = input('Введите ссылку: ')
     try:
-        if is_bitlink(url):
-            print(count_clicks(TOKEN, url))
+        if is_bitlink(url, TOKEN):
+            click_count = count_clicks(TOKEN, url)
+            print(f"Данной ссылкой воспользовались {click_count} раз")
         else:
             print(shorten_link(TOKEN, url))
-    except requests.exceptions.HTTPError:
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
         print('Введённая ссылка невалидна')
     except:
         print('Что-то пошло не так')

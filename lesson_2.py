1ef70b885a8601c6d782f3e57335af7aaabf1a00
Igor <92385445+ITerekhov@users.noreply.github.com>
2022-04-06 12:30:16 +0300

    Внесены изменения по правкам

--- lesson_2.py
+++ lesson_2.py
@@ -1,7 +1,8 @@
-import requests
-from urllib.parse import urlparse
-from dotenv import load_dotenv
 import os
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+import requests
 
 def shorten_link(requests_headers, url):
     request_url = 'https://api-ssl.bitly.com/v4/bitlinks'
@@ -14,8 +15,7 @@ def count_clicks(requests_headers, bitlink:str):
     request_url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
     response = requests.get(request_url, headers=requests_headers, params={'units': -1})
     response.raise_for_status()
-    click_count = response.json()['total_clicks']
-    return click_count
+    return response.json()['total_clicks']
 
 def is_bitlink(requests_headers, bitlink):
     '''Функция делает запрос к API сервиса, чтобы узнать, является ли url уже 
@@ -32,14 +32,13 @@ def main():
     try:
         response = requests.get(url)
         response.raise_for_status()
-        if response.ok:
-            parsed_url = urlparse(url)
-            bitlink = parsed_url.netloc + parsed_url.path
-            if is_bitlink(requests_headers, bitlink):
-                click_count = count_clicks(requests_headers, bitlink)
-                print(f"Данной ссылкой воспользовались {click_count} раз")
-            else:
-                print(shorten_link(requests_headers, url))
+        parsed_url = urlparse(url)
+        bitlink = parsed_url.netloc + parsed_url.path
+        if is_bitlink(requests_headers, bitlink):
+            click_count = count_clicks(requests_headers, bitlink)
+            print(f"Данной ссылкой воспользовались {click_count} раз")
+        else:
+            print(shorten_link(requests_headers, url))
     except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
         print('Введённая ссылка невалидна')
 
